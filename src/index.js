cedaccbbc31a3d9d3be19b6c15b4f7e1cb87a9f1
poyuKiryl <poyuki9@gmail.com>
2018-09-11 17:48:26 +0300

    regExp baby

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let checkArray=[],res=true,leftReg='\\((?:[^)(]+|\\([^)',rightReg='(]*\\))\\)*';
+
+    bracketsConfig.forEach((el,index)=>{
+        let regExp=`\\${el[0]}(?:[^)(]+|\\${el[0]}(?:[^)(]+|\\${el[0]}(?:[^)(]+|\\${el[0]}(?:[^)(]+|\\${el[0]}(?:[^)(]+|\\${el[0]}(?:[^)(]+|\\${el[0]}(?:[^)(]+|\\${el[0]}(?:[^)(]+|\\${el[0]}(?:[^)(]+|\\${el[0]}(?:[^)(]+|\\${el[0]}(?:[^)(]+|\\${el[0]}(?:[^)(]+|\\${el[0]}(?:[^)(]+|\\${el[0]}(?:[^)(]+|\\${el[0]}[^)(]*\\${el[1]})*\\${el[1]})*\\${el[1]})*\\${el[1]})*\\${el[1]})*\\${el[1]})*\\${el[1]})*\\${el[1]})*\\${el[1]})*\\${el[1]})*\\${el[1]})*\\${el[1]})*\\${el[1]})*\\${el[1]})*\\${el[1]}`;
+        let f=str.search(regExp);
+        let t;
+        let r=true;
+
+        while(r){
+            t=str.replace(new RegExp(regExp),'');
+            if(t.indexOf(regExp)===-1){
+                r=false
+            }
+
+        }
+        if(f===-1/*||t.indexOf(el[0])!==-1||t.indexOf(el[1])!==-1*/) res=false;
+    });
+
+    return res;
+};
